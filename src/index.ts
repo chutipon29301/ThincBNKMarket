703c9aabb3355cbe02fd3bcd6d5cc5e8c9d13de0
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-03-27 15:01:37 +0700

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log("Hello World");
-console.log('Hello');
+import * as express from "express";
+import * as morgan from "morgan";
+import { urlencoded, json } from "body-parser";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(express.static("./public"));
+
+app.use(urlencoded({
+    extended: true
+}));
+app.use(json());
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port ' + process.env.PORT);
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
-const path = require('path');
-const NodemonPlugin = require('nodemon-webpack-plugin');
+var path = require('path');
+var nodeExternals = require('webpack-node-externals');
+var NodemonPlugin = require('nodemon-webpack-plugin');
+
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new NodemonPlugin(),
-  ],
+    entry: './src/index.ts',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: ['ts-loader'],
+            enforce: 'pre',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    node: {
+        __dirname: false
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    plugins: [
+        new NodemonPlugin(),
+    ]
 };
