f2ce4bae5d07e34b49d86ec268875bc0f333787f
mingyanisa <mingignim@hotmail.com>
2018-03-27 17:46:08 +0700

    finish 2 pages

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,53 @@ app.use(urlencoded({
 }));
 app.use(json());
 
-app.set("view engine","pug");
+app.set("view engine", "pug");
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port ' + process.env.PORT);
 });
 
-app.get("/", (req,res) => {
-    res.status(200).render("index");
+app.get("/", (req, res) => {
+    res.status(200).render("index",{
+        products: [{
+            imgURL: '',
+            name: 'photoset1',
+            price: '40'
+        },
+        {
+            imgURL: '',
+            name: 'T-shirt',
+            price: '50'
+        },
+        {
+            imgURL: '',
+            name: 'Wrist-Band',
+            price: '30'
+        },{
+            imgURL: '',
+            name: 'PenLight',
+            price: '20'
+        }]
+    });
+});
+
+app.get("/product-detail", (req, res) => {
+    res.status(200).render("product-detail");
+});
+
+app.get("/cart", (req, res) => {
+    res.status(200).render("cart",{
+        products: [{
+            imgURL: '',
+            name: 'photoset',
+            price: '40',
+            quantity: '2'
+        },
+        {
+            imgURL: '',
+            name: 't-shirt',
+            price: '50',
+            quantity: '1'
+        }]
+    });
 });
